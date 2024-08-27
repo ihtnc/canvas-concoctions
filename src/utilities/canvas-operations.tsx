c18f6e617dbed0970e432bab3314fe513deb7001
ihtnc <ihopethisnamecounts@yahoo.com>
2024-08-27 14:54:55 +1000

    Removed unused code

--- src/utilities/canvas-operations.tsx
+++
@@ -1,7 +0,0 @@
-type CanvasOperationFunction = (canvas: HTMLCanvasElement) => void;
-
-export const clearFrame:CanvasOperationFunction = (canvas) => {
-  const { width, height } = canvas
-  const context = canvas.getContext('2d')
-  context?.clearRect(0, 0, width, height)
-}
